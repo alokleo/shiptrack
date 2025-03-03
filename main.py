e2174909e554ebfe94ff7f87ed2baf340f1ced05
Robert Paulson <admin@altostrat.com>
2025-03-03 05:37:45 +0530

    push to test50

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/discovery', methods=['GET'])
 def discovery():
     """
-    Provides discovery information for the shipping service.
+    Returns a JSON response containing details about the shipping service.
 
     Returns:
         JSON: A dictionary containing the service's name, version, owners, team, and organization.
@@ -20,30 +20,13 @@ def discovery():
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
-#Genertae code for error conditions
+# Generate error conditions
 @app.errorhandler(404)
 def not_found(error):
-    """
-    Handles 404 Not Found errors.
-
-    Args:
-        error: The error object.
-
-    Returns:
-        JSON: A dictionary containing an error message and status code.
-    """
-    return jsonify({'error': 'Not found', 'status_code': 404}), 404
+    return jsonify({'error': 'Not found'}), 404
 
 @app.errorhandler(500)
 def internal_server_error(error):
-    """
-    Handles 500 Internal Server errors.
+    return jsonify({'error': 'Internal Server Error'}), 500
 
-    Args:
-        error: The error object.
-
-    Returns:
-        JSON: A dictionary containing an error message and status code.
-    """
-    return jsonify({'error': 'Internal Server Error', 'status_code': 500}), 500
 
